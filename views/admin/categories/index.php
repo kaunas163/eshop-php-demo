c7c6107f88898ba4df4014e67854f1aeb7f8e52a
Ieva Jaksaityte <ieva.jaksaityte@gmail.com>
2018-12-03 00:03:12 +0200

    admin: visu kategoriju perziura

--- views/admin/categories/index.php
+++ views/admin/categories/index.php
@@ -1,5 +1,50 @@
 <?php include("../../../header.php"); ?>
 
-<p>Visos kategorijos</p>
+<h1 class="display-3">Prekių kategorijos</h1>
+<p class="lead">Šios kategorijos matomos vartotojams.</p>
+<hr class="my-4">
+<p>
+    <a href="new.php" class="btn btn-success">Nauja</a>
+</p>
+
+<?php
+
+$sql = "SELECT `id`, `title`, `description`, `parent_category_id` FROM `categories`";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    ?>
+    <table class="table table-striped table-hover">
+        <thead class="thead-dark">
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col" style="width: 60%">Kategorija</th>
+                <th scope="col">Veiksmai</th>
+            </tr>
+        </thead>
+        <tbody>
+    <?php
+    while($row = $result->fetch_assoc()) {
+        ?>
+            <tr>
+                <th scope="row"><?php echo $row['id']; ?></th>
+                <td><?php echo $row["title"]; ?></td>
+                <td>
+                    <a href="view.php?id=<?php echo $row["id"]; ?>" class="btn btn-primary">Peržiūrėti</a>
+                    <a href="edit.php?id=<?php echo $row["id"]; ?>" class="btn btn-warning">Atnaujinti</a>
+                    <a href="delete.php?id=<?php echo $row["id"]; ?>" class="btn btn-danger">Šalinti</a>
+                </td>
+            </tr>
+        <?php
+    }
+    ?>
+        </tbody>
+    </table>
+    <?php
+} else {
+    echo '<div class="col">Nieko nėra.</div>';
+}
+
+?>
 
 <?php include("../../../footer.php"); ?>
---
+++ views/admin/categories/new.php
@@ -0,0 +1,7 @@
+<?php include("../../../header.php"); ?>
+
+<h1 class="display-3">Nauja prekių kategorija</h1>
+<p class="lead">Koks nors paaiškinamasis tekstas.</p>
+<hr class="my-4">
+
+<?php include("../../../footer.php"); ?>
