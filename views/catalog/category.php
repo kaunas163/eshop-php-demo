73911e4006c53a6653b3c12b6f4b562e4b44474f
Ieva Jaksaityte <ieva.jaksaityte@gmail.com>
2018-12-04 01:57:12 +0200

    user can browse categories

--- views/catalog/category.php
+++ views/catalog/category.php
@@ -5,10 +5,22 @@
 <hr class="my-4">
 
 <div class="row">
+    <div class="col">
+        <h4>Kategorijos</h4>
+    </div>
+</div>
+
+<div class="row">
 
 <?php
 
-    $sql = "SELECT `id`, `title`, `description`, `parent_category_id` FROM `categories`";
+    $categoryId = 0;
+
+    if (isset($_GET['id'])) {
+        $categoryId = $_GET['id'];
+    }
+
+    $sql = "SELECT `id`, `title`, `description`, `parent_category_id` FROM `categories` WHERE parent_category_id=$categoryId";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
@@ -29,8 +41,49 @@
             <?php
         }
     } else {
-        echo '<div class="col">Nieko nėra.</div>';
+        echo '<div class="col"><p>Nieko nėra.</p></div>';
     }
+
+?>
+
+</div>
+
+<div class="row">
+    <div class="col">
+        <h4>Prekės</h4>
+    </div>
+</div>
+
+<div class="row">
+
+<?php
+
+$sql = "SELECT id, title, description, price FROM products WHERE category_id=$categoryId";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while($product = $result->fetch_assoc()) {
+        ?>
+            <div class="col-4">
+                <div class="card bg-light mb-3">
+                    <div class="card-header">
+                        <a href="./product.php?id=<?php echo $product['id']; ?>">
+                            <?php echo $product["title"]; ?>
+                        </a>
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text"><?php echo $product["description"]; ?></p>
+                    </div>
+                    <div class="card-footer text-muted">
+                        <p>Kaina: <?php echo $product['price']; ?></p>
+                    </div>
+                </div>
+            </div>
+        <?php
+    }
+} else {
+    echo '<div class="col"><p>Nieko nėra.</p></div>';
+}
 
 ?>
 
